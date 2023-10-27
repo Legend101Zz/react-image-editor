c1085e7399a251d7b958216992b1fbf4c9ab2451
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-10-27 17:07:50 +0530

    made the change design button working

--- src/components/Editor/start.jsx
+++ src/components/Editor/start.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Layouts/Navbar/Navbar.js";
 import { Grid, Paper, Button, Typography } from "@mui/material";
 import "./DesignSelection.css";
 
 function DesignSelection() {
+  const [userDesign, setUserDesign] = useState(null);
+
+  const handleDesignChange = (e) => {
+    // Handle the user's design upload and set it to the state
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setUserDesign(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
   return (
     <>
       <Navbar />
@@ -15,30 +28,39 @@ function DesignSelection() {
           </div>
           <div className="design-table-row">
             <div className="design-cell design-preview">
-              <img src="logo192.png" alt="User Design" />
-              <Button
-                variant="contained"
-                style={{
-                  paddingLeft: "10%",
-                  paddingRight: "10%",
-                  marginLeft: "5%",
-                  marginTop: "20%",
-                  backgroundColor: "#CBB428",
-                  borderRadius: "20px 0 0 20px",
-                }}
-              >
-                Change Design
-              </Button>
+              <img src={userDesign || "logo192.png"} alt="User Design" />
+              <label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  style={{ display: "none" }}
+                  onChange={handleDesignChange}
+                />
+                <Button
+                  variant="contained"
+                  style={{
+                    paddingLeft: "10%",
+                    paddingRight: "10%",
+                    marginLeft: "5%",
+                    marginTop: "20%",
+                    backgroundColor: "#CBB428",
+                    borderRadius: "20px 0 0 20px",
+                  }}
+                  component="span" // This makes the button act like a file input button
+                >
+                  Change Design
+                </Button>
+              </label>
             </div>
             <div className="design-cell categories">
               <div className="category">
                 <Button variant="contained">Shirts</Button>
                 <div className="category-images">
-                  <img src="t_shirt2.jpeg" alt="Shirt 1" />
+                  <img src="t_shirt.png" alt="Shirt 1" />
                   <Button variant="outlined" className="edit-button">
                     Edit
                   </Button>
-                  <img src="t_shirt2.jpeg" alt="Shirt 2" />
+                  <img src="t_shirt.png" alt="Shirt 2" />
                   <Button variant="outlined" className="edit-button">
                     Edit
                   </Button>
